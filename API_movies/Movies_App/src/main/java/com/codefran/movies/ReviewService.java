92b583c80b384c30638b8f9d134c391cecd756b9
franciscocastillo <castillor.franciscoj@gmail.com>
2024-03-14 23:21:14 -0400

    update reviews service, Fixed error on review array field on movie document of Mongodb

--- API_movies/Movies_App/src/main/java/com/codefran/movies/ReviewService.java
+++ API_movies/Movies_App/src/main/java/com/codefran/movies/ReviewService.java
@@ -1,5 +1,4 @@
 package com.codefran.movies;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+
 @Service
 public class ReviewService {
     @Autowired
@@ -16,17 +16,13 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public Review createReview(String reviewBody, String imdbId){
-        Review review = repository.insert(new Review(reviewBody));
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review.getId()))
+                .apply(new Update().push("reviews").value(review.getId()))
                 .first();
-
-
         return review;
-
     }
-
 }
